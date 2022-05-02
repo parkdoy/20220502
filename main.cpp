f6f0fcc1aedda25a0f688afd661ff9b8aaf96d58
parkdoyoung <swagee7@gmail.com>
2022-05-02 09:40:36 +0900

    정수형,실수형 사칙연산

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 
-
 int main()
 {
 	int a = 10;
-	int b=20;
+	int b = 20;
+
 	float fa = 10.f;
 	float fb = 20.f;
 
@@ -18,6 +18,6 @@ int main()
 	std::cout << fa - fb << std::endl;
 	std::cout << fa * fb << std::endl;
 	std::cout << fa / fb << std::endl;
-	std::cout << (int)fa % (int)fb << std::endl;
+	std::cout << (int)fa % (int)fb << std::endl; //실수형은 안된다. 그래서 정수형으로 형변환한거임.
 
 }
