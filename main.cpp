f15dd9c6b4355dc7abcd225e7dc313bcac32560c
parkdoyoung <swagee7@gmail.com>
2022-05-02 09:51:43 +0900

    return 0;

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 int main()
 {
+
 	int a = 10;
 	int b = 20;
 
@@ -20,4 +21,5 @@ int main()
 	std::cout << fa / fb << std::endl;
 	std::cout << (int)fa % (int)fb << std::endl; //실수형은 안된다. 그래서 정수형으로 형변환한거임.
 
+	return 0;
 }
